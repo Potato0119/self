9c67b76de78901e1fc51e0d9ebdcd1a817135bdd
Potato0119 <502ender502@gmail.com>
2023-09-24 19:00:44 +0800

    0.1.6

--- js/mobile.js
+++ js/mobile.js
@@ -15,12 +15,20 @@ function isMobileDevice() {
   }
   return false;
 }
-
-if (isMobileDevice()) {
-  document.querySelector("header img").style =
-    "position: absolute;transform: translate(-50%, 0);top: 0;left: 50%;width: 237.3px;height: 40px;";
-  document.querySelector(".mobile-nav").style = "visibility: visible;";
-  document.querySelector("header a").style = "visibility: hidden;";
-} else {
-  console.log("not mobile device");
-}
+setTimeout(() => {
+  if (isMobileDevice()) {
+    document.querySelector("header img").style =
+      "position: absolute;transform: translate(-50%, 0);top: 0;left: 50%;width: 237.3px;height: 40px;";
+    document.querySelector(".mobile-nav").style = "visibility: visible;";
+    document.querySelector(".headera").style = "visibility: hidden;";
+  } else {
+    document.querySelector("header img").style =
+      "position: absolute;transform: translate(-50%, 0);top: 0;left: 50%;width: 237.3px;height: 40px;";
+    document.querySelector(".mobile-nav").style = "visibility: visible;";
+    document.querySelector("#nav").style = "visibility: visible;";
+    document.querySelector(".headera1").style = "visibility: hidden;";
+    document.querySelector(".headera2").style = "visibility: hidden;";
+    document.querySelector(".headera3").style = "visibility: hidden;";
+    document.querySelector(".headera4").style = "visibility: hidden;";
+  }
+}, 1000);
