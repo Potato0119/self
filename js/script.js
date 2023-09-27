7f381db06fb990cb92cc550ef6f15215e05a7f4c
Potato0119 <502ender502@gmail.com>
2023-09-27 22:38:20 +0800

    0.5.0

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function run() {
+  setTimeout(() => {
+    document.querySelector(".main div").style = "height:100%;width:5px;";
+  }, 200);
+  setTimeout(() => {
+    document.querySelector(".main p").style = "left: 0px;";
+  }, 700);
+  setTimeout(() => {
+    document.querySelector(".mainp").style = "left: 0px;";
+  }, 1200);
+}
