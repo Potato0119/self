6918912f4b08dfffe34eb83d63282996752d4659
Potato0119 <502ender502@gmail.com>
2023-09-28 19:38:47 +0800

    0.5.2

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 function run() {
   setTimeout(() => {
     document.querySelector(".main div").style = "height:100%;width:5px;";
-  }, 200);
+  }, 500);
   setTimeout(() => {
     document.querySelector(".main p").style = "left: 0px;";
-  }, 700);
+  }, 1000);
   setTimeout(() => {
     document.querySelector(".mainp").style = "left: 0px;";
-  }, 1200);
+  }, 1500);
 }
