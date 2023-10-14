e6e5b70f6c59fa6a62b70f2464a57ed12c497af7
Potato0119 <502ender502@gmail.com>
2023-10-15 00:22:07 +0800

    0.8.1

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 function run() {
+  document.querySelector(".talkdiv p").textContent = "我是施心棠 不要懷疑!!";
   setTimeout(() => {
     document.querySelector(".main div").style = "height:100%;width:5px;";
   }, 500);
