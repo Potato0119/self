0aa8e563a78b7f6a26db962f7b2da71c91c60406
Potato0119 <502ender502@gmail.com>
2023-11-04 13:51:44 +0800

    0.10

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
+// 入場動畫
 function run() {
-  document.querySelector(".talkdiv p").textContent = "我是施心棠 不要懷疑!!";
+  document.querySelector(".talkdiv p").textContent = "我是施心棠";
   setTimeout(() => {
     document.querySelector(".main div").style = "height:100%;width:5px;";
   }, 500);
@@ -10,3 +11,13 @@ function run() {
     document.querySelector(".mainp").style = "left: 0px;";
   }, 1500);
 }
+
+// 手機選單開關
+const checkbox = document.getElementById("check");
+checkbox.addEventListener("change", function () {
+  if (this.checked) {
+    document.querySelector(".nav-ul").style.display = "block";
+  } else {
+    document.querySelector(".nav-ul").style.display = "none";
+  }
+});
